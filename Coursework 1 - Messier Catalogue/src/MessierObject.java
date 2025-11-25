5981937a614e4e86dd977dad9172064ee59332d0
alex1k1c <alexkait07@icloud.com>
2025-11-25 13:23:17 +0000

    adding more messier constructor tests

--- Coursework 1 - Messier Catalogue/src/MessierObject.java
+++ Coursework 1 - Messier Catalogue/src/MessierObject.java
@@ -1,8 +1,10 @@
 public class MessierObject {
     String messierNum;
+    String ncgNum;
     //constructor
-    public MessierObject(String m) {
+    public MessierObject(String m, String n) {
         messierNum = m;
+        ncgNum = n;
     }
 
 
@@ -10,8 +12,10 @@ public class MessierObject {
 
     public static void main(String[] args) {
         //testing instance creation
-        MessierObject test = new MessierObject("M1");
+        MessierObject test = new MessierObject("M1", "NGC 1952");
+        MessierObject test2 = new MessierObject("M2", "NGC 7089");
 
-        System.out.println("the first object is called "+test.messierNum);
+        System.out.println("the first object is called "+test.messierNum + ". the ncg number is "+ test.ncgNum);
+        System.out.println("the next object is called "+test2.messierNum + ". the ncg number is "+ test2.ncgNum);
     }
 }
