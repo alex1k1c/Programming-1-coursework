af86b44ebc0e38ebada5129f707d0e235017083e
alex1k1c <alexkait07@icloud.com>
2025-11-29 15:34:04 +0000

    - added a method to parse data from text file in order to store it properly. this includes splitting it up, removing quote marks and returning a clean version - added second constructor method to take 1 line of data from messier.txt as parameter then uses method to parse and store object as the 9 parts needed. right ascension and declination is temporarily held as 0.0 until i make the calculation to correctly store it.

--- Coursework 1 - Messier Catalogue/src/MessierObject.java
+++ Coursework 1 - Messier Catalogue/src/MessierObject.java
@@ -25,6 +25,37 @@ public class MessierObject {
         this.dec = dec;
     }
 
+
+    private static String[] parseLine(String line) {
+        String[] p = line.split(","); // splits the line into parts seperated by commas
+
+        String[] cleaned = new String[8];
+
+        for (int i = 0; i < 8; i++) {
+            cleaned[i] = p[i].trim().replace("\"", ""); //removes double quotes from parts
+        }
+
+        return cleaned; // new clean list of info
+    }
+    //constructor 2 - takes 1 whole line as parameter from text file
+    public MessierObject(String line) {
+        //String[] p = line.split(",");
+        //this function is needed, as you cannot put any code above 'this' - must be at top of constructo
+        //therefore function goes inside the 'this' statement
+        this(
+                parseLine(line)[0],
+                parseLine(line)[1],
+                parseLine(line)[2],
+                parseLine(line)[3],
+                parseLine(line)[4],
+                parseLine(line)[5],
+                parseLine(line)[6],
+                0.0, //temporary
+                0.0);//temporary
+    }
+
+
+
     //Accessor methods
     public String getMessierNum() {return messierNum;}
     public String getNgcIcNum() {return ngcIcNum;}
