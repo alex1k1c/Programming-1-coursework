d50a2a4e472db32dc81bd13a4919832f5dcff66b
Michael King <mk.michael.king201@gmail.com>
2025-12-04 23:07:17 +0000

    Question c: Least distant open cluster

--- Coursework 1 - Messier Catalogue/src/MessierProgram.java
+++ Coursework 1 - Messier Catalogue/src/MessierProgram.java
@@ -23,7 +23,7 @@ public class MessierProgram {
             }
         });
         //Loops through the sorted list printing each object
-        for (MessierObject obj : sortedList){
+        for (MessierObject obj : sortedList) {
             System.out.println(obj);
         }
         System.out.println();
@@ -37,20 +37,42 @@ public class MessierProgram {
         for (MessierObject o : catalogue.getMember()) {
             if (Objects.equals(o.getType(), "Globular cluster")) { //checking if the type is right
 
-                //if the type is right, magnitude is added to sum, count is recorded to divide both by eachother
+                //if the type is right, magnitude is added to sum, count is recorded to divide both by each other
                 double i = Double.parseDouble(o.getApparentMag());
                 sum += i;
-                count+= 1;
-                avgMagnitude = (sum/count);
+                count += 1;
+                avgMagnitude = (sum / count);
 
             }
 
 
         }
-        System.out.println("The average magnitude for all globular clusters is: "+ avgMagnitude);
-
-
+        System.out.println("The average magnitude for all globular clusters is: " + avgMagnitude);
+        System.out.println();
 
         //c
+        System.out.println("Least distant open cluster");
+
+        MessierObject closestOpen = null; //Stores closest open cluster
+        double minDistance = Double.MAX_VALUE; //Starting with the largest value so any real distance is smaller
+        //Loop through every MessierObject in list
+        for (MessierObject obj : list) {
+            if (obj.getType().toLowerCase().contains("open")) { //Checks if object is an open cluster
+                try {
+                    double dist = Double.parseDouble(obj.getDistance());
+                    //If distance is smaller than previous min distance, value is updated
+                    if (dist < minDistance) {
+                        minDistance = dist;
+                        closestOpen = obj;
+                    }
+                } catch (NumberFormatException e) {
+                }
+            }
+        }
+        //When loop finishes result is printed
+        if (closestOpen != null) {
+            System.out.println("The least distant open cluster is:");
+            System.out.println(closestOpen);
+        }
     }
 }
